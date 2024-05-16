d7829784fc509602bce2fe295a61f0503acaf87a
Diego A. <161983005+CodeDasg@users.noreply.github.com>
2024-05-15 21:02:27 -0500

    Modified the secret_config file

--- secret_config.py
+++ secret_config.py
@@ -1,4 +1,4 @@
-PGHOST='ep-patient-smoke-a5w5b6zw.us-east-2.aws.neon.tech'
+PGHOST='ep-tight-lake-a58i8qau.us-east-2.aws.neon.tech'
 PGDATABASE='calculatordb'
 PGUSER='calculatordb_owner'
-PGPASSWORD='XsGZxe4n7jtb'
+PGPASSWORD='i8zVRuqLwjN0'
